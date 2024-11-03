b772d7bfa10660c9a58ad96408e8c48c18b72afe
Eyal001 <eyal.zeitoun@epitech.eu>
2024-11-03 15:06:21 +0200

    [ADD] my mandatory exercises

--- Week1/Day1/Ex-XP/xp-ex-mandatory.py
+++ Week1/Day1/Ex-XP/xp-ex-mandatory.py
@@ -1,9 +1,56 @@
 #ex1
+print('Hello World \n' * 4)
 
-# print('Hello World')
+#ex2
+my_number= print((99**3) * 8)
 
-#ex2 
+#ex3
 
-#my_name = "Eyal"
-#print(Eyal)
-# TO THE CHECKER: I DIDN'T UNDERSTAND THIS EXERCISE
+# 5 < 3 It's False
+# 3 == 3 It's True
+# 3 == "3" It's False
+# "3" > 3 It's TypeError
+#"Hello" == "hello") It's False
+
+#ex4
+computer_brand = "Lenovo"
+print(f"I have a {computer_brand} computer")
+
+#ex5
+name = "Eyal"
+age = 26
+shoe_size = 43
+info = f"Hi, my name is {name} , I'm {age}, my shoe size is {shoe_size}"
+print(info) 
+
+#ex6
+
+a = 26
+b = 13
+if a > b:
+  print("Hello World")
+
+#ex7
+
+num = int(input("Type a number :"))
+if(num % 2 == 0):
+  print(f"{num} is even !")
+else:
+  print(f"{num} is odd !")
+
+#ex8
+
+my_name = "Eyal"
+
+name_usr= str(input("Type your name :"))
+if(my_name == name_usr):
+  print("HOLY @!/# we have the same name !!")
+else:
+  print("disapointing.. I prefer mine.")
+
+#ex9
+height = int(input("Type your height in centimeters :"))
+if(height > 145):
+  print("You are tall enough to ride")
+else:
+  print("You need to grow some more to ride")
