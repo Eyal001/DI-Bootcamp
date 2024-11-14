2b11152812420e3840ed97979652877b63e7f87b
Eyal001 <eyal.zeitoun@epitech.eu>
2024-11-14 16:33:26 +0200

    [DELETE] some comments

--- Week2/Day5/Ex-XP/ro_pa_sc/rock-paper-scissors.py
+++ Week2/Day5/Ex-XP/ro_pa_sc/rock-paper-scissors.py
@@ -1,9 +1,9 @@
 from game import Game
-RED = '\033[31m'        # Rouge pour le texte
-GREEN = '\033[32m'      # Vert pour le texte
-YELLOW = '\033[33m'     # Jaune pour le texte
-BLUE = '\033[34m'       # Bleu pour le texte
-RESET = '\033[0m'       # Réinitialise la couleur (par défaut)
+RED = '\033[31m'    
+GREEN = '\033[32m'     
+YELLOW = '\033[33m'     
+BLUE = '\033[34m'       
+RESET = '\033[0m'       
 
 
 def get_user_menu_choice():
