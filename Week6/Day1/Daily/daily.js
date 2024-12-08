0ea55d275bd6e0e96dfdebdfe2ab1e42f78cf9c7
Eyal001 <eyal.zeitoun@epitech.eu>
2024-12-08 17:36:23 +0200

    [UPDATE] uncomment some lines

--- Week6/Day1/Daily/daily.js
+++ Week6/Day1/Daily/daily.js
@@ -1,45 +1,45 @@
 // ex 1
 
-// function makeAllCaps(words) {
-//   return new Promise((resolve, reject) => {
-//     if (words.every((word) => typeof word === "string")) {
-//       const upperCasedWords = words.map((word) => word.toUpperCase());
-//       resolve(upperCasedWords);
-//     } else {
-//       reject("Not all elements in the array are strings");
-//     }
-//   });
-// }
+function makeAllCaps(words) {
+  return new Promise((resolve, reject) => {
+    if (words.every((word) => typeof word === "string")) {
+      const upperCasedWords = words.map((word) => word.toUpperCase());
+      resolve(upperCasedWords);
+    } else {
+      reject("Not all elements in the array are strings");
+    }
+  });
+}
 
-// function sortWords(words) {
-//   return new Promise((resolve, reject) => {
-//     if (words.length > 4) {
-//       const sortedWords = words.sort();
-//       resolve(sortedWords);
-//     } else {
-//       reject("The array doesn't have more than 4 words");
-//     }
-//   });
-// }
+function sortWords(words) {
+  return new Promise((resolve, reject) => {
+    if (words.length > 4) {
+      const sortedWords = words.sort();
+      resolve(sortedWords);
+    } else {
+      reject("The array doesn't have more than 4 words");
+    }
+  });
+}
 
-// //in this example, the catch method is executed
-// makeAllCaps([1, "pear", "banana"])
-//   .then((arr) => sortWords(arr))
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
+//in this example, the catch method is executed
+makeAllCaps([1, "pear", "banana"])
+  .then((arr) => sortWords(arr))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
-// //in this example, the catch method is executed
-// makeAllCaps(["apple", "pear", "banana"])
-//   .then((arr) => sortWords(arr))
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
+//in this example, the catch method is executed
+makeAllCaps(["apple", "pear", "banana"])
+  .then((arr) => sortWords(arr))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
-// //in this example, you should see in the console,
-// // the array of words uppercased and sorted
-// makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
-//   .then((arr) => sortWords(arr))
-//   .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
-//   .catch((error) => console.log(error));
+//in this example, you should see in the console,
+// the array of words uppercased and sorted
+makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
+  .then((arr) => sortWords(arr))
+  .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
+  .catch((error) => console.log(error));
 
 // ex 2
 const morse = `{
