a88b10f8460283e24ac4e1d60e561cc197a3a901
Eyal001 <eyal.zeitoun@epitech.eu>
2024-12-22 17:12:46 +0200

    [ADD] my Carousel daily challenge with React

---
+++ Week8/Day1/Daily/my-daily/src/App.jsx
@@ -0,0 +1,13 @@
+import CarouselComponent from "./Components/CarouselComponent";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Travel Carousel</h1>
+        <CarouselComponent />
+      </div>
+    </>
+  );
+}
+
+export default App;
